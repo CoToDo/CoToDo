793c2b3242b1a327dbb7447056f85a69266b4b08
Jindrich Zak <jindra.zak@gmail.com>
2018-04-17 18:54:36 +0200

    Login and registration forms use Bootstrap.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Table(name="app_users")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("mail")
  */
 class User implements UserInterface, \Serializable
 {
