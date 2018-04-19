b837cd3cd4c5b8a246449c231c5b2916be5b68d7
dusekpe2 <dusekpe2@fit.cvut.cz>
2018-04-20 01:20:37 +0200

    Added use for security in controllers

--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
  * @Route("/roles")
