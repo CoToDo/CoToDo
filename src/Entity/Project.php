4ab4fac199ef0e66f1dce3c00a2ff277e088678c
privrja <privrja2@fit.cvut.cz>
2018-05-03 11:43:25 +0200

    Migrate DB - project name unique

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -24,7 +24,7 @@ class Project
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $description;
 
