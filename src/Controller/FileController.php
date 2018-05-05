2ed9a4229bb22d7204dec91186c67c90851c3c92
dusekpe2 <dusekpe2@fit.cvut.cz>
2018-05-05 17:16:38 +0200

    Add security to filecontroller

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use App\Model\ExportModel;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class FileController extends Controller
 {
     /**
      * @Route("/export", name="file_export")
+     * @Security("has_role('ROLE_USER')")
      */
     public function export()
     {
