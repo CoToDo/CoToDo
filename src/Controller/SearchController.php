f7215529ccfbadbb9c31d474d2b92aff9c44cc36
dusekpe2 <dusekpe2@fit.cvut.cz>
2018-05-06 13:31:15 +0200

    Try to run searching

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Repository\ProjectRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SearchController extends Controller
 {
+    /**
+     * @Route("/search", name="search")
+     */
     public function index(ProjectRepository $projectRepository)
     {
         return $this->render('search/index.html.twig', [
@@ -19,12 +25,35 @@ class SearchController extends Controller
     /**
      * @Route("/searchProjects/{param}", name="search_projects")
      */
-    public function showProjects(ProjectRepository $projectRepository, $param){
+    public function showProjects(ProjectRepository $projectRepository, $param)
+    {
 
         return $this->render('search/show_projects.html.twig', [
             'controller_name' => 'SearchController',
-            'projects' =>$projectRepository->findProjectsMatch($param)
+            'projects' => $projectRepository->findProjectsMatch($param)
         ]);
     }
 
+    /**
+     * @Route("/tmp", name="tmp")
+     */
+    public function searchAction(ProjectRepository $projectRepository, Request $request) : Response
+    {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+//            $projectRepository->findProjectsMatch($form["search"]->getData())
+            echo("coje");
+
+            return $this->redirectToRoute('search_projects', ['param' => $form["search"]->getData()]);
+        }
+
+        return $this->render('search/in.html.twig', [
+            'form' => $form->createView(),
+            'projects' => $projectRepository->findAllSearch(),
+        ]);
+    }
 }
+
---
+++ src/Form/SearchType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('search')
+            ->setAction("searchProjects", array('search'))
+        ;
+    }
+
+}
