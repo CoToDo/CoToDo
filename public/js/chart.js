6fb429cce07f1640272dd79a9dfe3957dde4a365
privrja <privrja2@fit.cvut.cz>
2018-05-18 18:24:03 +0200

    Prepare chart for get data

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,20 +1,23 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var user = {{ user|json_encode|raw }};
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'bar',
+function graphTask(arr) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
 
-    // The data for our dataset
-    data: {
-        labels: autocomplete(document.getElementById("works"), user),
-        datasets: [{
-            label: "Work done on this task",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    },
+        // The data for our dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "Work done on this task",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: arr,
+                // data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        },
 
-    // Configuration options go here
-    options: {}
-});
+        // Configuration options go here
+        options: {}
+    });
+}
+
