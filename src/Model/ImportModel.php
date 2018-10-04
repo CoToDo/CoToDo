3fcaa58979c336567d25a3eff23fe8b3395416e6
privrja <privrja2@fit.cvut.cz>
2018-10-04 17:48:54 +0200

    Import#38
    + add ImportModel

---
+++ src/Model/ImportModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model;
+
+
+class ImportModel {
+
+    public function import() {
+
+    }
+
+    private function parse() {
+
+    }
+
+    private function saveToDatabse() {
+
+    }
+
+}
