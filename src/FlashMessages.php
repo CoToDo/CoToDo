70f592f83b418bc064dc05d2323eba4e17d31800
dusekpe2 <32452112+dusekpe2@users.noreply.github.com>
2018-10-24 00:36:53 +0200

    Bugs (#50)
    * Bugs #42
    
    exception pages
    project team not null
    
    * Bug #42
    
    Leader can't delete himself when he is the last leader in team. Also added flash messages.
    
    * Bugs #42
    
    Code modified to be nicer.

---
+++ src/FlashMessages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petr
+ * Date: 24/10/2018
+ * Time: 00:07
+ */
+
+namespace App;
+
+abstract class FlashMessages
+{
+    const WARNING = 'warning';
+    const DELETE_LEADER = 'leader';
+
+}
