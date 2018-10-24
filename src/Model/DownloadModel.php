1d6fcdb4d0fa1ceba9e7b0d574bf165976dd9491
dusekpe2 <32452112+dusekpe2@users.noreply.github.com>
2018-10-24 14:07:45 +0200

    Calendar1 (#52)
    * Calendar #44
    
    Make downloadable calendar with .ics format.
    
    * Calendar #44
    
    Take back last commit and added some strings to .ics file.

---
+++ src/Model/DownloadModel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petr
+ * Date: 24/10/2018
+ * Time: 11:19
+ */
+
+namespace App\Model;
+
+use App\Entity\Task;
+use App\Model\ICS;
+
+
+class DownloadModel
+{
+    public function downloadIcs(Task $task, $url) {
+        header('Content-Type: text/calendar; charset=utf-8');
+        header('Content-Disposition: attachment; filename=task.ics');
+
+        $ics = new ICS(array(
+            'description' => "priority " . $task->getPriority(),
+            'dtstart' => $task->getCreateDate()->format(('Y-m-d H:i:s')),
+            'dtend' => $task->getDeadline()->format(('Y-m-d H:i:s')),
+            'summary' => "Team: " . $task->getProject()->getTeam()->getName() . '\n'
+                        . "Project: " . $task->getProject()->getName() . '\n'
+                        . "Task: " . $task->getName(),
+            'url' => $url
+        ));
+
+        echo $ics->to_string();
+    }
+}
