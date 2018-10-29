ca0a1d8afa81be6cf21915cab0dbcc73ec6dfe97
privrja <privrja2@fit.cvut.cz>
2018-10-29 20:27:37 +0100

    Charts #37
    + add ajax - non functional

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,6 +1,6 @@
 function graphTask(arr, arr1) {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'bar',
 
@@ -11,7 +11,7 @@ function graphTask(arr, arr1) {
                 label: "Work done on this task",
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
-                data: arr,
+                data: loadTimes(),
                 // data: [0, 10, 5, 2, 20, 30, 45],
             }]
         },
@@ -19,5 +19,21 @@ function graphTask(arr, arr1) {
         // Configuration options go here
         options: {}
     });
+}
+
+function loadTimes() {
+    var data;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            data =  this.responseText;
+        }
+    };
+    console.log(window.location.href);
+    console.log(this.responseText);
+    xhttp.open("GET", window.location.href + "/times", true);
+    xhttp.send();
+    console.log(data);
+    console.log(this.readyState + " " + this.status);
 }
 
