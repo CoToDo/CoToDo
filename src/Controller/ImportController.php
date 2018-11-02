ac6ab0e13122cecd8237fe87d8b735f6d5a54a58
privrja <privrja2@fit.cvut.cz>
2018-11-02 18:50:25 +0100

    FileUploadFix
    + exception

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -19,10 +19,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ImportController extends Controller
-{
-    const CONTROLLER_NAME="controller_name";
-    const IMPORT_CONTROLLER="ImportController";
+class ImportController extends Controller {
+    const CONTROLLER_NAME = "controller_name";
+    const IMPORT_CONTROLLER = "ImportController";
     const WRONG = 'wrong';
     const MIME_TYPE_TEXT_PLAIN = "text/plain";
 
@@ -30,22 +29,23 @@ class ImportController extends Controller
      * @Route("/import", name="import")
      * @Security("has_role('ROLE_USER')")
      */
-    public function index(Request $request, ManagerRegistry $doctrine)
-    {
-        $ToDoTxtFile=new ToDoTxtFile();
+    public function index(Request $request, ManagerRegistry $doctrine) {
+        $ToDoTxtFile = new ToDoTxtFile();
         $form = $this->createFormBuilder($ToDoTxtFile)
             ->add('file', FileType::class, array('label' => 'Plain text file (txt)', 'attr' => array('accept' => 'text/plain')))
-            ->add('save', SubmitType::class, array('label' => 'Import', 'attr'=> array('class'=>'btn btn-large btn-primary')))
+            ->add('save', SubmitType::class, array('label' => 'Import', 'attr' => array('class' => 'btn btn-large btn-primary')))
             ->getForm();
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             try {
                 $file = $ToDoTxtFile->getFile();
-                if ($file->getClientMimeType() !== self::MIME_TYPE_TEXT_PLAIN) {
+                var_dump($file->getClientOriginalExtension() . " " + $file->getClientMimeType());
+                if ($file->getClientMimeType() !== self::MIME_TYPE_TEXT_PLAIN || $file->getClientOriginalExtension() !== 'txt') {
                     $this->flashMessageWrongMimeType();
-                    $this->renderPageWithErrorBeforeImport($form);
+                    return $this->renderPageWithErrorBeforeImport($form);
                 }
+                var_dump($file->getClientOriginalExtension() . " " + $file->getClientMimeType());
                 $fileName = md5(uniqid()) . '.' . $file->guessExtension();
                 $file->move($this->getParameter('file_directory'), $fileName);
                 $ToDoTxtFile->setFile($fileName);
@@ -53,13 +53,16 @@ class ImportController extends Controller
                 $import = new ImportModel($doctrine->getManager(), $this->getUser());
                 $txtWrongLines = $import->importFromFile($file->getPath() . "/" . $ToDoTxtFile->getFile());
                 $this->flashMessagesAfterImport($txtWrongLines);
+                return $this->render('import/import.html.twig', [
+                    self::CONTROLLER_NAME => self::IMPORT_CONTROLLER,
+                    self::WRONG => implode("\n", $txtWrongLines)
+                ]);
             } catch (\Exception $exception) {
-                return $this->renderPageWithErrorBeforeImport($form);
+                $this->flashMessageException($exception);
+                return $this->render('import/import.html.twig', [
+                    self::CONTROLLER_NAME => self::IMPORT_CONTROLLER,
+                ]);
             }
-            return $this->render('import/import.html.twig', [
-                self::CONTROLLER_NAME => self::IMPORT_CONTROLLER,
-                self::WRONG => implode("\n", $txtWrongLines)
-            ]);
         } else {
             return $this->renderPageWithErrorBeforeImport($form);
         }
@@ -110,4 +113,10 @@ class ImportController extends Controller
         );
     }
 
+    private function flashMessageException($ex) {
+        $this->addFlash(
+            'warning',
+            "Exception!" . $ex->getMessage()
+        );
+    }
 }
