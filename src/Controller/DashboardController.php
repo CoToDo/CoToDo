fe325783d0c848a8925df9bf6aa74fdd3a0ad900
privrja <privrja@gmail.com>
2018-11-28 18:22:40 +0100

    TimeLine #5
    + add task array and V data to view

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -10,11 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
-class DashboardController extends Controller
-{
-
+class DashboardController extends Controller {
     const TODAY_COLOR = "#aa0000";
     const OTHER_COLOR = "#000000";
+    const TEXT_SIZE = 10;
 
     /**
      * Render DashBoard view
@@ -24,8 +23,7 @@ class DashboardController extends Controller
      * @Route("/dashboard", name="dashboard")
      * @Security("has_role('ROLE_USER')")
      */
-    public function index(TaskRepository $taskRepository): Response
-    {
+    public function index(TaskRepository $taskRepository): Response {
         $user = $this->getUser();
         $dateTime = new \DateTime('now');
         $dateTime->setTimezone(new \DateTimeZone(date_default_timezone_get()));
@@ -36,17 +34,53 @@ class DashboardController extends Controller
         $friColor = ($dateTime->format("D") === "Fri") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
         $satColor = ($dateTime->format("D") === "Sat") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
         $sunColor = ($dateTime->format("D") === "Sun") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
-        $weekNo = $dateTime->format("W");
-        $newDate = new \DateTime();
-        $newDate->setTimezone(new \DateTimeZone(date_default_timezone_get()));
-        $newDate->setISODate($dateTime->format("Y"), $weekNo);
 
+        $monDate = $this->getMonday();
+
+        $tueDate = $this->getDaywithInterval(1);
+        $wedDate = $this->getDaywithInterval(2);
+        $thuDate = $this->getDaywithInterval(3);
+        $friDate = $this->getDaywithInterval(4);
+        $satDate = $this->getDaywithInterval(5);
+        $sunDate = $this->getDaywithInterval(6);
+        $monTasks = array();
+        $tueTasks = array();
+        $wedTasks = array();
+        $thuTasks = array();
+        $friTasks = array();
+        $satTasks = array();
+        $sunTasks = array();
+        foreach ($taskRepository->findMyTasksSortedByPriority($user->getId()) as $task) {
+            var_dump($task->getDeadline()->format('dMY'));
+            switch ($task->getDeadline()->format('dMY')) {
+                case $monDate->format('dMY'):
+                    $monTasks[] = $task;
+                    break;
+                case $tueDate->format('dMY'):
+                    $tueTasks[] = $task;
+                    break;
+                case $wedDate->format('dMY'):
+                    $wedTasks[] = $task;
+                    break;
+                case $thuDate->format('dMY'):
+                    $thuTasks[] = $task;
+                    break;
+                case $friDate->format('dMY'):
+                    $friTasks[] = $task;
+                    break;
+                case $satDate->format('dMY'):
+                    $satTasks[] = $task;
+                    break;
+                case $sunDate->format('dMY'):
+                    $sunTasks[] = $task;
+                    break;
+            }
+        }
 
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'tasks' => $taskRepository->findMyTasksSortedByPriority($user->getId()),
             'user' => $this->getUser(),
-            'monDate' => $newDate->format("d.m"),
             'monColor' => $monColor,
             'tueColor' => $tueColor,
             'wedColor' => $wedColor,
@@ -54,14 +88,43 @@ class DashboardController extends Controller
             'friColor' => $friColor,
             'satColor' => $satColor,
             'sunColor' => $sunColor,
-            'tueDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
-            'wedDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
-            'thuDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
-            'friDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
-            'satDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
-            'sunDate' => $newDate->add(new \DateInterval('P1D'))->format('d.m'),
+            'monDate' => $monDate->format('d.m'),
+            'tueDate' => $tueDate->format('d.m'),
+            'wedDate' => $wedDate->format('d.m'),
+            'thuDate' => $thuDate->format('d.m'),
+            'friDate' => $friDate->format('d.m'),
+            'satDate' => $satDate->format('d.m'),
+            'sunDate' => $sunDate->format('d.m'),
+            'monTasks' => $monTasks,
+            'tueTasks' => $tueTasks,
+            'wedTasks' => $wedTasks,
+            'thuTasks' => $thuTasks,
+            'friTasks' => $friTasks,
+            'satTasks' => $satTasks,
+            'sunTasks' => $sunTasks,
+            'monV' => sizeof($monTasks) * DashboardController::TEXT_SIZE,
+            'tueV' => sizeof($tueTasks) * DashboardController::TEXT_SIZE,
+            'wedV' => sizeof($wedTasks) * DashboardController::TEXT_SIZE,
+            'thuV' => sizeof($thuTasks) * DashboardController::TEXT_SIZE,
+            'friV' => sizeof($friTasks) * DashboardController::TEXT_SIZE,
+            'satV' => sizeof($satTasks) * DashboardController::TEXT_SIZE,
+            'sunV' => sizeof($sunTasks) * DashboardController::TEXT_SIZE,
         ]);
-
     }
 
+    private function getMonday() {
+        $dateTime = new \DateTime('now');
+        $dateTime->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+        $weekNo = $dateTime->format("W");
+        $monDate = new \DateTime();
+        $monDate->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+        $monDate->setISODate($dateTime->format("Y"), $weekNo);
+        return $monDate;
+    }
+
+    private function getDaywithInterval($interval) {
+        $monDate = $this->getMonday();
+        $monDate->add(new \DateInterval("P" . $interval . "D"));
+        return $monDate;
+    }
 }
