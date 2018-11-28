80128350d34ad7c18635bcd3605bc342c6de2a31
privrja <privrja@gmail.com>
2018-11-28 16:03:57 +0100

    TimeLine #5
    + data for timeline, dates and color

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,6 +12,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DashboardController extends Controller
 {
+
+    const TODAY_COLOR = "#aa0000";
+    const OTHER_COLOR = "#000000";
+
     /**
      * Render DashBoard view
      * @param TaskRepository $taskRepository
@@ -22,10 +26,37 @@ class DashboardController extends Controller
     public function index(TaskRepository $taskRepository): Response
     {
         $user = $this->getUser();
+        $dateTime = new \DateTime('now');
+        $dateTime->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+        $monColor = ($dateTime->format("D") === "Mon") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $tueColor = ($dateTime->format("D") === "Tue") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $wedColor = ($dateTime->format("D") === "Wed") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $thuColor = ($dateTime->format("D") === "Thu") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $friColor = ($dateTime->format("D") === "Fri") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $satColor = ($dateTime->format("D") === "Sat") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $sunColor = ($dateTime->format("D") === "Sun") ? DashboardController::TODAY_COLOR : DashboardController::OTHER_COLOR;
+        $weekNo = $dateTime->format("W");
+        $newDate = new \DateTime();
+        $newDate->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+        $newDate->setISODate($dateTime->format("Y"), $weekNo);
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
             'tasks' => $taskRepository->findMyTasksSortedByPriority($user->getId()),
-            'user' => $this->getUser()
+            'user' => $this->getUser(),
+            'monDate' => $newDate->format("d.m"),
+            'monColor' => $monColor,
+            'tueColor' => $tueColor,
+            'wedColor' => $wedColor,
+            'thuColor' => $thuColor,
+            'friColor' => $friColor,
+            'satColor' => $satColor,
+            'sunColor' => $sunColor,
+            'tueDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
+            'wedDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
+            'thuDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
+            'friDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
+            'satDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
+            'sunDate' => $newDate->add(new \DateInterval('P1D'))->format('d m'),
         ]);
 
     }
