ee8799618e004edf3003a9ecde1d1c7b8cf9e3ba
dusekpe2 <dusekpe2@fit.cvut.cz>
2018-12-02 17:00:06 +0100

    Synchronization #39
    Added flash messages when found nothing in drive. More effective code.

--- src/Controller/SynchronizationController.php
+++ src/Controller/SynchronizationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\FlashMessages;
 use App\Model\ExportModel;
 use Google_Client;
 use Google_Service_Drive;
@@ -27,8 +28,6 @@ class SynchronizationController extends Controller
      */
     public function index() {
         $this->getDefaultPath();
-        $folderId = null;
-        $fileId = null;
         $client = new Google_Client();
         $client->setAuthConfig($this->defaultPath . self::CREDENTIALS_JSON);
         $client->addScope(array(Google_Service_Drive::DRIVE_METADATA, Google_Service_Drive::DRIVE_FILE,
@@ -39,62 +38,6 @@ class SynchronizationController extends Controller
 
             $exportModel = new ExportModel();
             print $exportModel->exportUser($this->getUser());
-//            $optParams = array(
-//                'pageSize' => 10,
-//                'fields' => 'nextPageToken, files(id, name)'
-//            );
-//
-//            $files = $drive->files->listFiles($optParams);
-//            if (count($files->getFiles()) == 0) {
-////                print "No files found.\n";
-//            } else {
-////                print "Files:\n";
-//                foreach ($files->getFiles() as $file) {
-//                    if($file->getName() == "CoToDo"){
-//                        $folderId = $file->getId();
-////                        printf("%s (%s)\n", $file->getName(), $file->getId());
-//                    }
-//                    //printf("%s (%s)\n", $file->getName(), $file->getId());
-//                }
-//            }
-//
-//            if($folderId == null){
-//                $folderMetadata = new Google_Service_Drive_DriveFile(array(
-//                    'name' => 'CoToDo',
-//                    'mimeType' => 'application/vnd.google-apps.folder'));
-//
-//                $folderId = $drive->files->create($folderMetadata, array(
-//                    'fields' => 'id'))->getId();
-//
-//            }
-//
-//
-//            foreach ($files->getFiles() as $file) {
-//                if($file->getName() == "todo.txt"){
-//                    $parents = $drive->files->get($file->getId(), array('fields' => 'parents'));
-//
-//                    foreach ($parents->parents as $parent) {
-////                        print $parent;
-//                        if($parent == $folderId) $fileId = $file->getId();
-//                    }
-//
-//                }
-//            }
-//
-//            if($fileId == null){
-//                $exportModel = new ExportModel();
-//                 $content = $exportModel->exportUser($this->getUser());
-//
-//                $fileMetadata = new Google_Service_Drive_DriveFile(array(
-//                    'name' => 'todo.txt',
-//                    'parents' => array($folderId)
-//                ));
-//
-//                $drive->files->create($fileMetadata, array(
-//                    'data' => $content,
-//                    'fields' => 'id'))->getId();
-//            }
-
 
         } else {
             return $this->redirectToRoute("sync_auth");
@@ -106,6 +49,7 @@ class SynchronizationController extends Controller
 
     /**
      * @Route("/sync/auth", name="sync_auth")
+     * @Security("has_role('ROLE_USER')")
      */
     public function auth() {
         $this->getDefaultPath();
@@ -127,11 +71,11 @@ class SynchronizationController extends Controller
 
     /**
      * @Route("/sync/export", name="sync_export")
+     * @Security("has_role('ROLE_USER')")
      */
     public function export() {
         $this->getDefaultPath();
-        $folderId = null;
-        $fileId = null;
+        $cotodo = null;
         $client = new Google_Client();
         $client->setAuthConfig($this->defaultPath . self::CREDENTIALS_JSON);
         $client->addScope(array(Google_Service_Drive::DRIVE_METADATA, Google_Service_Drive::DRIVE_FILE,
@@ -140,69 +84,65 @@ class SynchronizationController extends Controller
             $client->setAccessToken($_SESSION['access_token_drive']);
             $drive = new Google_Service_Drive($client);
 
-            $optParams = array(
+            $optParamsCotodo = array(
+                'q' => "name='CoToDo'",
                 'pageSize' => 10,
                 'fields' => 'nextPageToken, files(id, name)'
             );
 
-            $files = $drive->files->listFiles($optParams);
-            if (count($files->getFiles()) == 0) {
-//                print "No files found.\n";
-            } else {
-//                print "Files:\n";
-                foreach ($files->getFiles() as $file) {
-                    if($file->getName() == "CoToDo"){
-                        $folderId = $file->getId();
-//                        printf("%s (%s)\n", $file->getName(), $file->getId());
-                    }
-                    //printf("%s (%s)\n", $file->getName(), $file->getId());
-                }
-            }
 
-            if($folderId == null){
+            $cotodoFiles = $drive->files->listFiles($optParamsCotodo);
+            if (count($cotodoFiles->getFiles()) == 0) {
                 $folderMetadata = new Google_Service_Drive_DriveFile(array(
                     'name' => 'CoToDo',
                     'mimeType' => 'application/vnd.google-apps.folder'));
 
-                $folderId = $drive->files->create($folderMetadata, array(
-                    'fields' => 'id'))->getId();
+                $cotodo = $drive->files->create($folderMetadata, array(
+                    'fields' => 'id'));
+            } else if (count($cotodoFiles->getFiles()) == 1) {
+                $cotodo = $cotodoFiles->getFiles()[0];
+            } else {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Too much files found! Check your drive and keep just one CoToDo folder!'
+                );
 
+                return $this->redirectToRoute("synchronization");
             }
 
+            $optParamsTodo = array(
+                'q' => "'".$cotodo->getId(). "' in parents and name='todo.txt'",
+                'pageSize' => 10,
+                'fields' => 'nextPageToken, files(id, name)'
+            );
 
-            foreach ($files->getFiles() as $file) {
-                if($file->getName() == "todo.txt"){
-                    $parents = $drive->files->get($file->getId(), array('fields' => 'parents'));
-
-                    foreach ($parents->parents as $parent) {
-//                        print $parent;
-                        if($parent == $folderId) $fileId = $file->getId();
-                    }
-
-                }
-            }
-
+            $todoFiles = $drive->files->listFiles($optParamsTodo);
 
             $exportModel = new ExportModel();
             $content = $exportModel->exportUser($this->getUser());
 
-            if($fileId == null){
-
+            if (count($todoFiles->getFiles()) == 0) {
 
                 $fileMetadata = new Google_Service_Drive_DriveFile(array(
                     'name' => 'todo.txt',
-                    'parents' => array($folderId)
+                    'parents' => array($cotodo->getId())
                 ));
 
                 $drive->files->create($fileMetadata, array(
                     'data' => $content,
                     'fields' => 'id'))->getId();
-            } else {
+
+            } else if (count($todoFiles->getFiles()) == 1) {
                 $emptyFile = new Google_Service_Drive_DriveFile();
 
-//                print $content;
-                $drive->files->update($fileId, $emptyFile, array(
+                $drive->files->update($todoFiles->getFiles()[0]->getId(), $emptyFile, array(
                     'data' => $content));
+            } else {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Too much todo.txt files found! Check your drive and keep just one todo.txt file in CoToDo folder!'
+                );
+
             }
 
         } else {
@@ -211,4 +151,75 @@ class SynchronizationController extends Controller
         return $this->redirectToRoute("synchronization");
     }
 
+
+    /**
+     * @Route("/sync/import", name="sync_import")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function import() {
+        $this->getDefaultPath();
+        $cotodo = null;
+        $client = new Google_Client();
+        $client->setAuthConfig($this->defaultPath . self::CREDENTIALS_JSON);
+        $client->addScope(array(Google_Service_Drive::DRIVE_METADATA, Google_Service_Drive::DRIVE_FILE,
+            Google_Service_Drive::DRIVE, self::GOOGLE_SERVICE_DRIVE_ORDER));
+        if (isset($_SESSION['access_token_drive']) && $_SESSION['access_token_drive']) {
+            $client->setAccessToken($_SESSION['access_token_drive']);
+            $drive = new Google_Service_Drive($client);
+
+            $optParamsCotodo = array(
+                'q' => "name='CoToDo'",
+                'pageSize' => 10,
+                'fields' => 'nextPageToken, files(id, name)'
+            );
+
+
+            $cotodoFiles = $drive->files->listFiles($optParamsCotodo);
+            if (count($cotodoFiles->getFiles()) == 0) {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Nothing to import! Files for import must in CoToDo folder and be named todo.txt!'
+                );
+                return $this->redirectToRoute("synchronization");
+            } else if (count($cotodoFiles->getFiles()) == 1) {
+                $cotodo = $cotodoFiles->getFiles()[0];
+            } else {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Too much files found! Check your drive and keep just one CoToDo folder!'
+                );
+
+                return $this->redirectToRoute("synchronization");
+            }
+
+            $optParamsTodo = array(
+                'q' => "'".$cotodo->getId(). "' in parents and name='todo.txt'",
+                'pageSize' => 10,
+                'fields' => 'nextPageToken, files(id, name)'
+            );
+
+            $todoFiles = $drive->files->listFiles($optParamsTodo);
+
+            if (count($todoFiles->getFiles()) == 0) {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Nothing to import! Files for import must in CoToDo folder and be named todo.txt!'
+                );
+                return $this->redirectToRoute("synchronization");
+
+            } else if (count($todoFiles->getFiles()) == 1) {
+
+            } else {
+                $this->addFlash(
+                    FlashMessages::INFO,
+                    'Too much todo.txt files found! Check your drive and keep just one todo.txt file in CoToDo folder!'
+                );
+
+            }
+
+        } else {
+            return $this->redirectToRoute("sync_auth");
+        }
+        return $this->redirectToRoute("synchronization");
+    }
 }
